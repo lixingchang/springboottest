7ebbd6743ed9e966cec9c68c53af8d852ee10f19
lixingchang <707098845>
2020-06-16 09:35:35 +0800

    sprinboot-item

---
+++ src/main/java/com/lxc/springboot/SpringBoot02ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.lxc.springboot;
+
+import com.lxc.springboot.bean.Person;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@SpringBootApplication
+public class SpringBoot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ConfigApplication.class, args);
+    }
+
+}
